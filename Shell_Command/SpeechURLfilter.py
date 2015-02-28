e127ac01fad7d7f9c894cceed8c456a0c4ac6d1f
Joshua G. Mausolf <Joshua@joshuas-imac.attlocal.net>
2015-02-28 15:31:20 -0600

    Updates to package.

--- Shell_Command/SpeechURLfilter.py
+++
@@ -1,9 +0,0 @@
-import csv 
-with open('speechurls.csv', 'rU') as f:
-    for row in csv.reader(f):
-        if 'remarks-president' in row[0]:
-            with open('remarks-president_urls.csv','a') as f1: f1.write("{}\n".format(row[0]))
-        elif 'remarks-first-lady' in row[0]:
-            with open('remarks-first-lady_urls.csv', 'a') as f2: f2.write("{}\n".format(row[0]))
-        else:
-            pass
