ad326d945bfd74d000aff9908780f8fbe7890129
User <Joshua@joshuas-imac.attlocal.net>
2015-03-02 15:29:45 -0600

    Adding new shell scripts to do a year by year pull.

---
+++ Shell_Command/RUN2.sh
@@ -0,0 +1,20 @@
+
+## ____________ FIRST_STAGE - Run All Initial Bash Scripts ______________ ##
+
+bash 2009.sh
+
+
+
+bash 2010-pt1.sh
+bash 2010-pt2.sh
+
+
+bash 2011.sh
+bash 2012.sh
+bash 2013.sh
+bash 2014.sh
+bash 2015.sh
+bash 2009-2015.sh
+
+## ____________ SECOND_STAGE - Clean Year Folders Bash Scripts ______________ ##
+
